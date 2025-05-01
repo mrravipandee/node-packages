631be527d0aae28d78453192ff165e983c5aeab3
Ravi Pandey <imravipanday@gmail.com>
2025-05-01 12:39:50 +0530

    update text-to-speech-mp3/package.json

--- text-to-speech-mp3/dist/index.js
+++ text-to-speech-mp3/dist/index.js
@@ -1,40 +1,83 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.textToSpeech = textToSpeech;
-const googleTTS = require('google-tts-api');
-const fs_1 = __importDefault(require("fs"));
-const https_1 = __importDefault(require("https"));
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var __async = (__this, __arguments, generator) => {
+  return new Promise((resolve, reject) => {
+    var fulfilled = (value) => {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var rejected = (value) => {
+      try {
+        step(generator.throw(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
+    step((generator = generator.apply(__this, __arguments)).next());
+  });
+};
+
+// src/index.ts
+var index_exports = {};
+__export(index_exports, {
+  textToSpeech: () => textToSpeech
+});
+module.exports = __toCommonJS(index_exports);
+var import_fs = __toESM(require("fs"));
+var import_https = __toESM(require("https"));
+var googleTTS = require("google-tts-api");
 function textToSpeech(text, fileName) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const url = googleTTS.getAudioUrl(text, {
-                lang: 'en',
-                slow: false,
-                host: 'https://translate.google.com',
-            });
-            const file = fs_1.default.createWriteStream(`${fileName}.mp3`);
-            https_1.default.get(url, (res) => {
-                res.pipe(file);
-                file.on('finish', () => {
-                    file.close();
-                    console.log('✅ Audio saved successfully!');
-                });
-            });
-        }
-        catch (err) {
-            console.error('❌ Error generating audio:', err);
-        }
-    });
+  return __async(this, null, function* () {
+    try {
+      const url = googleTTS.getAudioUrl(text, {
+        lang: "en",
+        slow: false,
+        host: "https://translate.google.com"
+      });
+      const file = import_fs.default.createWriteStream(`${fileName}.mp3`);
+      import_https.default.get(url, (res) => {
+        res.pipe(file);
+        file.on("finish", () => {
+          file.close();
+          console.log("\u2705 Audio saved successfully!");
+        });
+      });
+    } catch (err) {
+      console.error("\u274C Error generating audio:", err);
+    }
+  });
 }
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  textToSpeech
+});
--- text-to-speech-mp3/dist/index.mjs
+++ text-to-speech-mp3/dist/index.mjs
@@ -1,3 +1,9 @@
+var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
+  get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
+}) : x)(function(x) {
+  if (typeof require !== "undefined") return require.apply(this, arguments);
+  throw Error('Dynamic require of "' + x + '" is not supported');
+});
 var __async = (__this, __arguments, generator) => {
   return new Promise((resolve, reject) => {
     var fulfilled = (value) => {
@@ -20,23 +26,23 @@ var __async = (__this, __arguments, generator) => {
 };
 
 // src/index.ts
-import googleTTS from "google-tts-api";
 import fs from "fs";
 import https from "https";
-function textToSpeech(text, filename = "output.mp3", lang = "en") {
+var googleTTS = __require("google-tts-api");
+function textToSpeech(text, fileName) {
   return __async(this, null, function* () {
     try {
       const url = googleTTS.getAudioUrl(text, {
-        lang,
+        lang: "en",
         slow: false,
         host: "https://translate.google.com"
       });
-      const file = fs.createWriteStream(filename);
+      const file = fs.createWriteStream(`${fileName}.mp3`);
       https.get(url, (res) => {
         res.pipe(file);
         file.on("finish", () => {
           file.close();
-          console.log(`\u2705 Saved MP3 as ${filename}`);
+          console.log("\u2705 Audio saved successfully!");
         });
       });
     } catch (err) {
